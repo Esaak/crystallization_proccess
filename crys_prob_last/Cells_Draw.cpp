a249ffb5380bb8b732ffa9c8bffc2162a798362d
Esaak <gerasimov.ia@phystech.edu>
2022-04-12 13:28:11 +0300

    after delete

--- crys_prob_last/Cells_Draw.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "Cells_Draw.h"
-Cells_Draw::Cells_Draw(Cells &cell) {
-    if(cell.Get_state_color())
-        circle[0] = sf::Vertex(sf::Vector2f(  cell.Get_coordinates().first, cell.Get_coordinates().second), color_crystal);
-    else
-        circle[0] = sf::Vertex(sf::Vector2f(  cell.Get_coordinates().first, cell.Get_coordinates().second), color_free);
-}
