c26ecca16c71a43045dbb44761fd71aa4bfe02c3
Esaak <gerasimov.ia@phystech.edu>
2022-04-23 08:00:02 +0300

    clean

---
+++ cmake-build-debug/CMakeFiles/first_attempt.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/informatika/crystallization_proccess/Cells.cpp" "D:/informatika/crystallization_proccess/cmake-build-debug/CMakeFiles/first_attempt.dir/Cells.cpp.obj"
+  "D:/informatika/crystallization_proccess/Cells_Draw.cpp" "D:/informatika/crystallization_proccess/cmake-build-debug/CMakeFiles/first_attempt.dir/Cells_Draw.cpp.obj"
+  "D:/informatika/crystallization_proccess/Map.cpp" "D:/informatika/crystallization_proccess/cmake-build-debug/CMakeFiles/first_attempt.dir/Map.cpp.obj"
+  "D:/informatika/crystallization_proccess/Map_Draw.cpp" "D:/informatika/crystallization_proccess/cmake-build-debug/CMakeFiles/first_attempt.dir/Map_Draw.cpp.obj"
+  "D:/informatika/crystallization_proccess/main.cpp" "D:/informatika/crystallization_proccess/cmake-build-debug/CMakeFiles/first_attempt.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "D:/informatika/SFML/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
